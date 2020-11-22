ac5560153833076a71d81b62adf1e28960eea047
YangHui <2411046022@qq.com>
2020-11-22 15:56:59 +0800

    20221122

---
+++ PID/pid.c
@@ -0,0 +1,52 @@
+#include "self.h"
+
+// 左侧PWM指针，右侧PWM指针，左侧目标速度，右侧目标速度，清除PID控制记录
+void CaculatePWM_PID(int16_t *PWM_Left, int16_t *PWM_Right, int16_t LeftTargetSpeed, int16_t RightTargetSpeed, int ClearFlag)
+{
+    extern uint16_t LeftSpeed, RightSpeed;
+    extern const double Ki, Kd, Kp;
+    static int32_t IntLeftSpeed = 0;
+    static int32_t IntRightSpeed = 0;
+    static int16_t LastLeftSpeed = 0;
+    static int16_t LastRightSpeed = 0;
+    if (ClearFlag) // 变换状态，重置所有的参数
+    {
+        IntLeftSpeed = 0;
+        IntRightSpeed = 0;
+        LastLeftSpeed = 0;
+        LastRightSpeed = 0;
+        return;
+    }
+    int32_t DiffLeftSpeed, DiffRightSpeed;
+    DiffLeftSpeed = 0;
+    DiffRightSpeed = 0;
+    RST_TIM2_3CNT();            // 清空TIM3/5->CNT寄存器
+    SysTick_DelaySync(30);      // 延迟30ms，这里的定时器是硬件定时器不会和中断冲突
+    LeftSpeed = GET_TIM3CNT();  // 获取左边速度
+    RightSpeed = GET_TIM2CNT(); // 获取右边速度
+    // 比例
+    int32_t DeltaLeftSpeed = LeftTargetSpeed - LeftSpeed;
+    int32_t DeltaRightSpeed = RightTargetSpeed - RightSpeed;
+    // 积分
+    IntLeftSpeed += DeltaLeftSpeed;
+    IntRightSpeed += DeltaRightSpeed;
+    // 微分
+    if (LastLeftSpeed != 0)
+    {
+        DiffLeftSpeed = LeftSpeed - LastLeftSpeed;
+        DiffRightSpeed = RightSpeed - LastRightSpeed;
+    }
+    LastLeftSpeed = LeftSpeed;
+    LastRightSpeed = RightSpeed;
+    *PWM_Left = Kp * DeltaLeftSpeed + Ki * IntLeftSpeed + Kd * DiffLeftSpeed;
+    *PWM_Right = Kp * DeltaRightSpeed + Ki * IntRightSpeed + Kd * DiffRightSpeed;
+    if (*PWM_Left < 0)
+        PWM_Left = 0;
+    if (*PWM_Right < 0)
+        PWM_Right = 0;
+    if (*PWM_Left > 9000)
+        *PWM_Left = 9000;
+    if (*PWM_Right > 9000)
+        *PWM_Right = 9000;
+    return;
+}
